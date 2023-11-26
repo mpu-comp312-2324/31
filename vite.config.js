5952496d1e44ffb5b9469a6ee976bbab346e552f
kylerhou <144279926+kylerhou@users.noreply.github.com>
2023-11-26 22:10:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    vue()
-  ]
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: true,
+    minify: 'esbuild',
+  },
+  server: {
+    port: 3000,
+  },
 });
-
